8b8f71c5b5505f71fa422c1a6692e7c09a51dd87
SANTHOSH V <145917701+SANTHOSH-VJ@users.noreply.github.com>
2026-08-14 10:45:38 +0530

    feat: add sitemap configuration for site metadata

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ export const dynamic = "force-static";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://santhosh-vj.github.io",
+      url: "https://santhosh-vj.github.io/",
       lastModified: new Date("2026-07-23"),
       changeFrequency: "weekly",
       priority: 1.0,
